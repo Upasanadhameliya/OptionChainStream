abc80d4cec3dbd89a0c5cf4afdaa79a2a207bb9c
Upasana <upasana.dhameliya@gmail.com>
2021-05-26 10:01:11 +0530

    Removed the keys from main program

--- access.py
+++ access.py
@@ -2,10 +2,10 @@ from optionchain_stream import OptionChain
 
 
 if __name__ == "__main__":
-	OptionStream = OptionChain("ydq5afgjqoqvj0up", "mlwdiwz24dnwpsu9jtozht4n4c5zncjb", "b24RIKYeP4Ym5aiwB3NNV3d23xhNJpxn",
-                    "ITC", "2021-05-27")#SBIN
+	OptionStream = OptionChain("<api-key-here>", "<api-secret-here>", "<acess-token-here>",
+                    "ITC", "2021-05-27")
 
-	OptionStream.sync_instruments() #51091) "24275970"
+	OptionStream.sync_instruments() #Uncomment once
 
 	StreamData = OptionStream.create_option_chain() #51213)
 	for data in StreamData:
